dc1a58df2eb616d26154fa431bd0c593a25f2b83
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-04 20:36:36 +0200

    added progress bar and some fixes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 "use strict";
 
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity( '.main-carousel', {
   // options
   cellAlign: 'left',
-  contain: true
-});
-
-var flkty = new Flickity( '.main-carousel', {
+  contain: true,
   hash: true,
+ });
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
